47e267fe1d16c782b1d830fa472dc0bf99b0a64c
johnhungerford <hungerfordjustice@gmail.com>
2019-03-25 19:51:02 -0400

    Improved hash function for hash table implementation

---
+++ hash-table.js
@@ -0,0 +1,32 @@
+class HashTable {
+    constructor(size = 993) {
+        this.buckets = new Array(size);
+        this.size = size;
+    }
+
+    hash(key) {
+        let hashedKey = 0;
+        let i;
+        for (i = 0; i < key.length; i += 5) {
+            for (let j = 0; j < 5 && i + j < key.length; j++) {
+                hashedKey += key.charCodeAt(j) * (64 ^ j);
+            }
+        }
+
+        return hashedKey % this.size;
+    }
+
+    set(key, value) {
+        const index = this.hash(key);
+        if(!Array.isArray(this.buckets[index])) this.buckets[index] = [];
+        this.buckets[index].push([key,value]);
+        return index;
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+        for (let i in bucket) if (bucket[i][0] === key) return bucket[i][1];
+        return undefined;
+    }
+}
